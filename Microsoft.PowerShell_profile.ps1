b10821bd389832107a2943fa8353d6882f5f35fc
James Smith <sup3rlativ3@gmail.com>
2024-05-13 17:03:00 +1000

    fix: profile loading

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,14 @@
 Import-Module posh-git, Terminal-Icons
 
+$env:POSH_AZURE_ENABLED = $true
+
 $WinTermFolder = Get-ChildItem "$($Env:UserProfile)\appdata\local\packages" -Filter Microsoft.WindowsTerminal*
 $OMPTheme = "$($WinTermFolder.FullName)\LocalState\SysOp.omp.json"
 
-oh-my-posh init pwsh --config $OMPTheme | Invoke-Expression
-oh-my-posh init powershell --config $OMPTheme | Invoke-Expression
+if (Test-Path $OMPTheme) {
+    $Shell = oh-my-posh get shell
+    oh-my-posh init $Shell --config $OMPTheme | Invoke-Expression
 
-$env:POSH_AZURE_ENABLED = $true
+} else {
+    Write-Warning "Oh-my-posh theme file not found: $OMPTheme"
+}
